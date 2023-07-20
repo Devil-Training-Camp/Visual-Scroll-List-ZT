865b4ec57998043f26601724dd2a45673a521514
zhaot97 <17088097497@163.com>
2023-07-20 16:26:46 +0800

    测试分支合并

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ for (let i = 0; i < 1000; i++) {
     data.push({ id: i, value: i })
 }
 let { dataList } = Mock.mock({
-    'dataList|50': [
+    'dataList|500': [
         {
             'id|+1': 1,
             title: '@ctitle',
